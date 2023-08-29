aaa8aad78be43a34730dbd5d5fb43b743fd33b2a
TedWB <127158120+TedWB@users.noreply.github.com>
2023-08-28 20:17:11 -0700

    Add files via upload

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -1,18 +1,64 @@
 import csv 
 import os
 
-files_to_load=os.path.join("Resources", "budget_data.csv")
+files_to_load=os.path.join("PyBank/Resources", "budget_data.csv")
 files_to_output=os.path.join("analysis", "budget_analysis")
 
 total_months=0
 months_of_change= []
-net_change_list=[]
-greatest_increase=["", 0]
-greatest_decrease=["", 9999999999999999999]
-total_net=0
+total_budget = 0
+changes = []
+dates = []
 
-with open(files_to_load) as financial_data:
-    reader = csv.reader(financial_data)
-    for row in reader: 
-        print(row)
+
+budgets_path = os.path.join("PyBank/Resources","budget_data.csv")
+with open(budgets_path, "r") as budgets_csv:
+    budgets = csv.reader(budgets_csv, delimiter= ",")
+
+    header = next(budgets)
+    print(header)
+    
+ 
+    first_row = next(budgets)
+    total_months += 1
+    total_budget = total_budget + int(first_row[1])
+    compare = int(first_row[1])
+ 
+    for row in budgets:
+
         
+        total_months += 1
+
+ 
+        total_budget = total_budget + int(row[1])
+
+        dates.append(row[0])
+
+   
+        change = int(row[1]) - compare
+ 
+        changes.append(change)
+
+        compare = int(row[1])
+    
+max_increase = max(changes)
+
+max_decrease = min(changes)
+
+max_index = changes.index(max_increase)
+max_month = dates[max_index]
+
+min_index = changes.index(max_decrease)
+min_month = dates[min_index]
+
+total_changes = sum(changes)
+avg_change = total_changes / len(changes)
+
+print("Financial Analysis\n----------------------------")
+print(f'Total Months: {total_months}')
+print(f'Total: ${total_budget}')
+print(f'Average Change: ${round(avg_change)}')
+print(f'Greatest Increase in Profits: {max_month}  ${max_increase}')
+print(f'Greatest Decrease in Profits: {min_month}  ${max_decrease}')
+
+output = os.path.join("Resources", "results.txt")
--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -1,6 +1,74 @@
 import csv 
 
-with open("Resources/election_data.csv","r") as e:
+with open("/Users/teddybadley/Class Folder/Challenges/Python-Challenge/PyPoll/Resources/election_data.csv","r") as e:
     reader = csv.reader(e,delimiter=",")
+
+    voterids=[] 
+    counties=[] 
+    candidates=[] 
+    candidatenames=[] 
+    totaleachcan=[] 
+    resultprintcan=[] 
+    totaleachcanperc=[] 
+
+    line_count=0
+    winnervotes=0
+    loservotes=0
+    loop1=0
+    loop2=0
+    loop3=0
+    loop4=0
+    
     for row in reader: 
-        print(row)
+        voterid=row[0] 
+        county=row[1] 
+        candidate=row[2] 
+        voterids.append(voterid) 
+        counties.append(county) 
+        candidates.append(candidate) 
+
+    line_count= len(voterids)
+    
+candidatenames.append(candidates[0])
+
+for loop1 in range (line_count-1):
+    if candidates[loop1+1] != candidates[loop1] and candidates[loop1+1] not in candidatenames:
+        candidatenames.append(candidates[loop1+1])
+
+n=len(candidatenames)  
+
+for loop2 in range (n): 
+    totaleachcan.append(candidates.count(candidatenames[loop2])) 
+
+loservotes=line_count 
+
+for loop3 in range(n):
+    totaleachcanperc.append(f'{round((totaleachcan[loop3]/line_count*100), 4)}%') 
+    if totaleachcan[loop3]>winnervotes: 
+        winner=candidatenames[loop3]
+        winnervotes=totaleachcan[loop3]
+    if totaleachcan[loop3]<loservotes: 
+        loser=candidatenames[loop3]
+        loservotes=totaleachcan[loop3]
+
+for loop4 in range(n):
+    resultprintcan.append(f'{candidatenames[loop4]}: {totaleachcanperc[loop4]} ({totaleachcan[loop4]})')
+
+resultlines='\n'.join(resultprintcan) 
+
+analysis=f'\
+Election Results\n\
+----------------------------\n\
+Total Votes: {line_count}\n\
+----------------------------\n\
+{resultlines}\n\
+----------------------------\n\
+Winner: {winner} :)\n\
+Last: {loser} :(\n\
+----------------------------\n'
+
+print(analysis) 
+
+file1=open("pypoll.txt","w") 
+file1.writelines(analysis) 
+file1.close() 
